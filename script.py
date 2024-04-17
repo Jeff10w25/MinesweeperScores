9ac84c479efcb4bd6fbd888f475a51e4181acab6
Thanathorn Suwanrasamee <thanatorns.2545@gmail.com>
2024-04-17 22:56:16 +0700

    update scores

--- script.py
+++ script.py
@@ -10,7 +10,26 @@ for filename in os.listdir(source):
     if os.path.isfile(filepath):
         
         """Expert"""
-        if re.search("Exp_NF", filepath):
+        if re.search("Exp_([0-999])", filepath):
+            dest_exp = os.path.join(dest, "exp/untyped/")
+            if re.search("Exp_(3[0-9])", filepath):
+                shutil.copy(filepath, os.path.join(dest_exp, "30-39/"))
+            elif re.search("Exp_(4[0-9])", filepath):
+                shutil.copy(filepath, os.path.join(dest_exp, "40-49/"))
+            elif re.search("Exp_(5[0-9])", filepath):
+                shutil.copy(filepath, os.path.join(dest_exp, "50-59/"))
+            elif re.search("Exp_(6[0-9])", filepath):
+                shutil.copy(filepath, os.path.join(dest_exp, "60-69/"))
+            elif re.search("Exp_(7[0-9])", filepath):
+                shutil.copy(filepath, os.path.join(dest_exp, "70-79/"))
+            elif re.search("Exp_(8[0-9])", filepath):
+                shutil.copy(filepath, os.path.join(dest_exp, "80-89/"))
+            elif re.search("Exp_(9[0-9])", filepath):
+                shutil.copy(filepath, os.path.join(dest_exp, "90-99/"))       
+            elif re.search("Exp_([10-99][0-9])", filepath):
+                shutil.copy(filepath, os.path.join(dest_exp, "100+/"))
+                
+        elif re.search("Exp_NF", filepath):
             dest_exp_nf = os.path.join(dest, "exp/NF/")
             if re.search("Exp_NF_(3[0-9])", filepath):
                 shutil.copy(filepath, os.path.join(dest_exp_nf, "30-39/"))
@@ -47,24 +66,6 @@ for filename in os.listdir(source):
                 shutil.copy(filepath, os.path.join(dest_exp_fl, "90-99/"))       
             elif re.search("Exp_FL_([10-99][0-9])", filepath):
                 shutil.copy(filepath, os.path.join(dest_exp_fl, "100+/"))
-                
-        elif re.search("Exp_([0-999])", filepath):
-            dest_exp = os.path.join(dest, "exp/untyped/")
-            if re.search("Exp_(3[0-9])", filepath):
-                shutil.copy(filepath, os.path.join(dest_exp, "30-39/"))
-            elif re.search("Exp_(4[0-9])", filepath):
-                shutil.copy(filepath, os.path.join(dest_exp, "40-49/"))
-            elif re.search("Exp_(5[0-9])", filepath):
-                shutil.copy(filepath, os.path.join(dest_exp, "50-59/"))
-            elif re.search("Exp_(6[0-9])", filepath):
-                shutil.copy(filepath, os.path.join(dest_exp, "60-69/"))
-            elif re.search("Exp_(7[0-9])", filepath):
-                shutil.copy(filepath, os.path.join(dest_exp, "70-79/"))
-            elif re.search("Exp_(8[0-9])", filepath):
-                shutil.copy(filepath, os.path.join(dest_exp, "80-89/"))
-            elif re.search("Exp_(9[0-9])", filepath):
-                shutil.copy(filepath, os.path.join(dest_exp, "90-99/"))       
-            elif re.search("Exp_([10-99][0-9])", filepath):
-                shutil.copy(filepath, os.path.join(dest_exp, "100+/"))  
+                  
                 
                     
